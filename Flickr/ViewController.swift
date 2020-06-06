f3643bd664a32b99e8a7e8f0ddbfc03af03d4321
Tracktor <evgenholkin@gmail.com>
2020-06-06 15:45:22 +0300

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Евгений Холкин on 06.06.2020.
+//  Copyright © 2020 Евгений Холкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
